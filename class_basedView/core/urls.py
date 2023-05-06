579bf086eff9e9448738f99dd8b9381259489c41
hyena <mesutkee@gmail.com>
2023-05-06 12:26:23 +0630

    changes in url and in database and template

--- class_basedView/core/urls.py
+++ class_basedView/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import PublisherListView,PublisherDetailView,BookList
+from .views import PublisherListView,PublisherDetailView,BookList,PublisherBookList
 
 urlpatterns = [
     path("publishers/", PublisherListView.as_view()),
     path("publisherDetail<int:pk>/", PublisherDetailView.as_view()),
     path("bookList/",BookList.as_view()),
+    path("book/<publisher>/",PublisherBookList.as_view()),
 ]
